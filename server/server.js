32fca2b3773540813f1f3232c3e1e275f1beb727
amnashoaib25 <amnachishti25@gmail.com>
2026-07-20 16:10:59 +0500

    Check Railway port

--- server/server.js
+++ server/server.js
@@ -38,8 +38,8 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
+console.log("PORT ENV =", process.env.PORT);
+
 app.listen(PORT, () => {
-    console.log(
-        `🚀 Server Running On Port ${PORT}`
-    );
+    console.log(`🚀 Server Running On Port ${PORT}`);
 });
